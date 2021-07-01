b3aba6c6aec3826b70d6ec1c723ed4e3d52a7144
Tobias Hernandez <tob3.14@gmail.com>
2021-07-01 14:04:03 -0400

    Changes made from grade feedback.

--- js/script.js
+++ js/script.js
@@ -11,28 +11,27 @@ project 1 - A Random Quote Generator
 ***/
 
 let quotes = [];
-let set_timer = setInterval(printQuote, 4000);  // method to automatically generate a new quote every 5 seconds
 let old_quote = '';
 
 quotes = [{
   quote: "A man's mind, stretched by new ideas, may never return to its original dimensions.",
   source:"Oliver Wendell Holmes Jr.",
   citation:"",
-  year:'',
+  year:'1904',
   tag:'education'
 },
 {
   quote: "He who opens a school door, closes a prison.",
   source:"Victor Hugo",
   citation:"",
-  year:'',
+  year:'1896',
   tag:'education'
 },
 {
   quote: "The only person who is educated is the one who has learned how to learn …and change.",
   source:"Carl Rogers",
   citation:"",
-  year:'',
+  year:'1968',
   tag:'education'
 },
 {
@@ -45,7 +44,7 @@ quotes = [{
 {
   quote: `Keep away from people who try to belittle your ambitions. 
   Small people always do that, but the really great make you feel that you, too, can become great.`,
-  source:"",
+  source:"Mark Twain",
   citation:`Mark Twain at Your Fingertips”, Edited by Caroline Thomas Harnsberger, 
   Quote Page 354, Cloud, Inc., Beechhurst Press, Inc., New York. (Verified on paper) `,
   year:'1948',
@@ -93,12 +92,7 @@ return bgColor;
 
 function printQuote()  {
   
-  clearInterval(set_timer);  //resets the 5 second counter;
   let newQuote = getRandomQuote();
-
-  while (newQuote == old_quote)  {  //check to make sure not to repeat quotes
-    printQuote();
-  } 
 
   let html = `<p class="quote"> ${newQuote['quote']} </p> 
               <p class="quote"> ${newQuote['source']}`;
@@ -106,17 +100,17 @@ function printQuote()  {
   if (newQuote['citation'])  {                                         //if citation exists
     html += `<span class="citation"> ${newQuote['citation']} </span>`
   }
-  if (newQuote['date'])  {                                             //if date exists
-    html += `<span class="date"> ${newQuote['date']} </span>` 
+  if (newQuote['year'])  {                                             //if year exists
+    html += `<span class="year"> ${newQuote['year']} </span>` 
   }
   if (newQuote['tag'])  {                                         //tagged
-    html += `<span class="tag"> tag> ${newQuote['tag']} </span>`
+    html += `<span class="tag"> tagged as: ${newQuote['tag']} </span>`
   }
   html += `</p>`;
   
+
   document.body.style.background = random_bg_color();  //trigger a random background color
-  set_timer;   //begins the clock for 5 second wait
-  old_quote = newQuote;  //will be used to check if quotes repeat when called again
+  
 
     return document.getElementById('quote-box').innerHTML = html;   //sends final html code to page
 }
@@ -124,5 +118,5 @@ function printQuote()  {
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
+setInterval(printQuote, 4000);   //begins the clock for 5 second wait between quotes
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
